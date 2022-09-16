eb42e433c467f0b6dd9d0f8309e9cf2e2051c2f9
Thaci12 <thaciana12@gmail.com>
2022-09-15 21:02:23 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+// serve para escrever mensagem no console
+// string = textos
+// number = números
+console.log(5)
+console.log(43.76)
+// boolean = boleano verdadeiro/falso
+console.log(true)
+console.log(false)
+
+// variaveis
+console.log("estudando variaveis")
+let mensagem = "soulcode bc 25 jfs" // LET SÓ SERVE P CRIAR A VRIAVEL
+
+console.log(mensagem)
+mensagem = "novo mundo"
+console.log(mensagem)
+
+
